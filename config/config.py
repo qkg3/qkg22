41ab33aca22f8bbea9582010b23206b8886d9f8f
ابو ايان <161139660+jxhxgh85@users.noreply.github.com>
2024-09-29 05:17:39 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -13,26 +13,26 @@ admins = {}
 
 #------------------------ Important Stuff 🤎 -----------------------
 
-API_ID = int(getenv("API_ID", "8934899"))
-API_HASH = getenv("API_HASH", "bf3e98d2c351e4ad06946b4897374a1e")
-BOT_TOKEN = getenv("BOT_TOKEN", "5204315005:AAGUB2q0QafBUwjDPGRAOcruwhUdqyXVlz4")
+API_ID = int(getenv("API_ID", "23077975"))
+API_HASH = getenv("API_HASH", "45407fde510529dc2041ce03b0f36a1d")
+BOT_TOKEN = getenv("BOT_TOKEN", "6499198312:AAFMUSlhIdOGBGP01r6JkTe1_NggL_083Ag")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "600"))
-STRING_SESSION = getenv("STRING_SESSION", "AgAZiHdAd4EOX2_TFaotq0BvHmvyfNnuzW1BXj_K4vIjrDbv_c2Ii7HvAyDU-kjCHAQsEvvY-AsNtiGCQ000WasBQ6qvB4wV5Ez7XZQyNmqbe2Cyg0DUTLHGFqKx-cuZ6RuYIla3TFYNx02fRE1TK_cYmdZr2KC4PvH80JM_zSzCju99osQ8xTQGI7L4wsZmOGD7ACCZpbGt_3WM-nalZCq4gS37ubEmu1pCLc7GE5NVkyUYyG1fERP8LRisXehFuf5Ly-ZOZ8e98sr0tYBRKfLsTODFsGVWRVIYvyZ4KstWO9O54MAm5eKkasktNoBSv49KSMPiBrtmbiNFKBXTi8loAAAAAVYfLGoA")
+STRING_SESSION = getenv("STRING_SESSION", "AgCIVfMARteM6ZNQf3Wv-Ztek72l7LM9sNTV8fGHByX6i8l2WyqRV8_c64zzp6egst1Y2n0F1DEjao9D9AKr8cjFovEWCR5khB9paH3AR1B0UGc-pXw084zceUAQSeXbKsXNtN8uqduRmfJFEAYI6FZ1uQR7fhevTFYUeJAGKFlzAyYhIb0ZZ_QFQ3tB52RyIt_4hb-3n3Y8OjSFLVBHlj3aRSXeaTu9BTpl3dNDlDRlprQhBbb2n5nrlUfOuO22yIRP1dVXhs1w-r6ZBq6dZgKrTaBBH5Pr8yZs9IugpryZHHBPFIfqMYM3Xd1LRLBFx8syRWcj-aQsH5YPYWSlFecvSUBCjQAAAAEv9SzoAA")
 BOT_USERNAME = getenv("BOT_USERNAME", "LROBOT")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5099564264").split()))
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "1854384004").split())
+    map(int, getenv("OWNER_ID", "5099564264").split())
 )  # Input type must be interger
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "30"))
 
 #•••••••••••••••••••••••• Mongodb Url Stuff & Loggroupid •••••••••••
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001847569598")) 
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002428993908")) 
 
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
 #________________________ Updates  & Music bot name________________
-NETWORK = getenv("NETWORK", "xl444")
-GROUP = getenv("GROUP", "xl444")
+NETWORK = getenv("NETWORK", "AFTU2")
+GROUP = getenv("GROUP", "AFTU2")
 BOT_NAME = getenv("BOT_NAME", "Music")
 BANNED_USERS = filters.user()
 
